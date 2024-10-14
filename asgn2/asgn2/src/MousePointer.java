439a7beb863a329688ec4f70cd42f4ee20037d00
sfkaplan <sfkaplan@calpoly.edu>
2024-10-14 16:54:48 -0700

    idk

--- asgn2/asgn2/src/MousePointer.java
+++ asgn2/asgn2/src/MousePointer.java
@@ -1,3 +1,6 @@
-public class MousePointer {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+public class MousePointer implements MouseMotionListener {
 
 }
