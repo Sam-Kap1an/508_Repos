77eabd8ececd6cb8dd050660736ba57a79f02917
laitchis <laitchis@calpoly.edu>
2024-10-14 15:15:17 -0700

    started

--- asgn2/asgn2/src/App.java
+++ asgn2/asgn2/src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Head head = new Head();
+        Thread head_thread = new Thread(head);
+
+        head_thread.start();
+        
+
+
+        
     }
 }
--- asgn2/asgn2/src/Head.java
+++ asgn2/asgn2/src/Head.java
@@ -1,3 +1,33 @@
-public class Head {
+import java.awt.*; 
+public class Head implements Runnable {
+
+    private int head_x;
+    private int head_y;
+    private int mouse_x;
+    private int mouse_y;
+    PointerInfo pi = MouseInfo.getPointerInfo(); 
+
+
+    public Head() {
+        this.head_x = 400;
+        this.head_y = 300;
+        this.mouse_x = 0;
+        this.mouse_y = 0;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("head is running!");
+        while (true) { 
+
+            Point p = pi.getLocation(); 
+            this.mouse_x = (int) p.getX();
+            this.mouse_y = (int) p.getY();
+            System.out.println(this.mouse_x + ", " + this.mouse_y);
+        }
+    }
+
+    
+
 
 }
